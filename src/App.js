08fc62e46e1a08e33e45c55997299f2af1e47ab8
Meric3434 <rapsodi34@gmail.com>
2024-05-20 03:59:31 +0300

    İlk commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Kart from './Kart'; // Kart bileşenini import edin
+import arkaPlan from './assets/background.png';
 import './App.css';
+import Button from '@mui/material/Button';
+
+
 
 function App() {
+  
+  
+  // Kart değerleri ve türleri
+  const değerler = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const türler = ['♥', '♦', '♣', '♠'];
+
+  // Tüm kartları oluşturun ve state'e kaydedin
+  const [kartlar, setKartlar] = useState(
+    değerler.flatMap(değer => türler.map(tür => ({ değer, tür, seçili: false })))
+  );
+
+  const resetKartlar = () => {
+    setKartlar(kartlar.map(kart => ({ ...kart, seçili: false })));
+  };
+
+  
+
+
+  // Kartları türlerine göre sırala
+  kartlar.sort((a, b) => {
+    const türSırası = ['♠','♥','♣','♦']; // Sıralama önceliği
+    return türSırası.indexOf(a.tür) - türSırası.indexOf(b.tür);
+  });
+
+  const kartSeç = (değer, tür) => {
+    setKartlar(
+      kartlar.map(kart =>
+        kart.değer === değer && kart.tür === tür
+          ? { ...kart, seçili: !kart.seçili }
+          : kart
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+     style={{
+        backgroundImage: `url(${arkaPlan})`,
+        backgroundSize: '100% 100%',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '100vh',
+        display: 'block',
+        flexDirection: 'row'
+       
+      }}
+    >
+      <Button 
+        variant="contained" 
+        onClick={resetKartlar}
+        sx={{ position:'fixed', top: 500, right: 0, color: '#ffd740'}}
+      >
+        Reset
+      </Button>
+      {/* Tüm kartları map ile oluşturun */}
+      {kartlar.map(kart => (
+        <Kart
+          key={kart.değer + kart.tür}
+          değer={kart.değer}
+          tür={kart.tür}
+          seçili={kart.seçili}
+          onKartSeç={kartSeç}
+        />
+      ))}
     </div>
   );
 }
